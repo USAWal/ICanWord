f9ec5123ec7da8bdf33828af0e759bd8e8979076
Valeriy Utyaganov <usawal@gmail.com>
2012-11-28 19:29:37 +0400

    Implemented filling in wanted definitions

--- src/de/zilant/icanword/Main.java
+++ src/de/zilant/icanword/Main.java
@@ -1,5 +1,7 @@
 package de.zilant.icanword;
 
+import java.util.LinkedList;
+
 import android.app.ExpandableListActivity;
 import android.app.LoaderManager.LoaderCallbacks;
 import android.content.ContentResolver;
@@ -7,9 +9,9 @@ import android.content.CursorLoader;
 import android.content.Loader;
 import android.content.res.Resources;
 import android.database.Cursor;
+import android.database.MatrixCursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.widget.SimpleCursorAdapter;
 import android.widget.SimpleCursorTreeAdapter;
 
 public class Main extends ExpandableListActivity implements LoaderCallbacks<Cursor> {
@@ -29,7 +31,6 @@ public class Main extends ExpandableListActivity implements LoaderCallbacks<Curs
 			
 			@Override
 			protected Cursor getChildrenCursor(Cursor groupCursor) {
-				// TODO Auto-generated method stub
 				return null;
 			}
 		});
@@ -43,7 +44,36 @@ public class Main extends ExpandableListActivity implements LoaderCallbacks<Curs
 
 	@Override
 	public void onLoadFinished(Loader<Cursor> loder, Cursor data) {
-		((SimpleCursorTreeAdapter) getExpandableListAdapter()).changeCursor(data);
+		SimpleCursorTreeAdapter adapter = (SimpleCursorTreeAdapter) getExpandableListAdapter();
+		MatrixCursor groupCursor = new MatrixCursor(new String[] {
+				data.getColumnName(ID_INDEX),
+				data.getColumnName(WORD_INDEX)
+		});
+		LinkedList<MatrixCursor> childrenCursors = new LinkedList<MatrixCursor>();
+		String word = null;
+		if(data.moveToFirst())
+		{
+			do {
+				String dataWord = data.getString(WORD_INDEX);
+				if(!dataWord.equals(word)) {
+					word = dataWord;
+					groupCursor.addRow(new Object[] {groupCursor.getCount(), word});
+					childrenCursors.add(new MatrixCursor(new String[] {
+							data.getColumnName(ID_INDEX),
+							data.getColumnName(PART_OF_SPEECH_INDEX),
+							data.getColumnName(DEFINITION_INDEX)
+					}));
+				}
+				childrenCursors.getLast().addRow(new Object[] {
+						data.getLong(ID_INDEX),
+						data.getString(PART_OF_SPEECH_INDEX),
+						data.getString(DEFINITION_INDEX)
+				});
+			} while(data.moveToNext());
+		}
+		adapter.setGroupCursor(groupCursor);
+		for(int cursorIndex = 0; cursorIndex < childrenCursors.size(); cursorIndex++)
+			adapter.setChildrenCursor(cursorIndex, childrenCursors.get(cursorIndex));
 	}
 
 	@Override
@@ -88,6 +118,11 @@ public class Main extends ExpandableListActivity implements LoaderCallbacks<Curs
 		"explanation",
 		"need_to_memorize"
 	};
+	protected static final int ID_INDEX = 0;
+	protected static final int WORD_INDEX = 1;
+	protected static final int PART_OF_SPEECH_INDEX = 2;
+	protected static final int DEFINITION_INDEX = 3;
+	protected static final int IS_WANTED_INDEX = 4;
 	
 	private Uri uri;
 
